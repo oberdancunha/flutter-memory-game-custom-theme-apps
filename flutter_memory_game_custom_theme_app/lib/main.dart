c07856b76a93cb07a8a199bbe7dd7041430604aa
oberdancunha <oberdancunha@gmail.com>
2022-02-26 09:49:21 -0300

    Only portraitUp device orientation.

--- flutter_memory_game_custom_theme_app/lib/main.dart
+++ flutter_memory_game_custom_theme_app/lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_memory_game_custom_theme_dependency_module/flutter_memory_game_custom_theme_dependency_module.dart';
 
 import 'presentation/core/app_module.dart';
 import 'presentation/core/app_widget.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+    ],
+  );
   runApp(
     ModularApp(
       module: AppModule(),
